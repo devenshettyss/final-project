e320c819cd6cf866672e8405097fbce1df5aa68b
devenshettyss <practice.devenshetty@gmail.com>
2021-09-03 12:37:39 +0000

    crud operation and invalid question exception class added

---
+++ src/main/java/com/tcs/springassesment/exception/InvalidQuestionException.java
@@ -0,0 +1,11 @@
+package com.tcs.springassesment.exception;
+
+
+
+public class InvalidQuestionException extends RuntimeException{
+	public InvalidQuestionException(String message) {
+		
+	super(message);
+	
+	}
+}
--- src/main/java/com/tcs/springassesment/repository/IUserRepository.java
+++ src/main/java/com/tcs/springassesment/repository/IUserRepository.java
@@ -1,12 +1,9 @@
 package com.tcs.springassesment.repository;
 
 import org.springframework.data.repository.CrudRepository;
-
+import com.tcs.springassesment.entity.Question;
 import com.tcs.springassesment.entity.User;
 
-
 public interface IUserRepository extends CrudRepository<User, Integer> {
-
-	
 
 }
